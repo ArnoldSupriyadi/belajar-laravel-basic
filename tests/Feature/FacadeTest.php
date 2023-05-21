dc19010e85edd45b3cdab029f93a04ac1e378d9c
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-05-21 11:12:47 +0700

    masih belajar

--- tests/Feature/FacadeTest.php
+++ tests/Feature/FacadeTest.php
@@ -4,9 +4,11 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use League\Flysystem\Config;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
+use function Spatie\Ignition\ErrorPage\config;
 
 class FacadeTest extends TestCase
 {
@@ -23,5 +25,29 @@ class FacadeTest extends TestCase
         self::assertEquals($firstName1, $firstName2);
 
         var_dump(Config::all());
+    }
+
+     public function testConfigDependecy()
+    {
+        $this = $this->app->make('config');
+        $firstName3 = config('contoh.author.first');
+
+        $firstName1 = config('contoh.author.first');
+        $firstName2 = Config::get('contoh.author.first');
+
+        self::assertEquals($firstName1, $firstName2);
+
+        var_dump(Config::all());
+    }
+
+    public function testFacadeMock()
+    {
+        Config::shouldReceive('get')
+            ->with('contoh.author.first')
+            ->andReturn('Eko Keren');
+
+            $firstName = Config::get('contoh.author.first');
+
+            self::assertEquals('Eko Keren', $firstName);
     }
 }
